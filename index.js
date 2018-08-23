b086f08ddbb1d50053dfc7a3c1e9222670f2b065
gabora93 <gabora93@gmail.com>
2018-08-23 13:26:35 -0700

    final commit uncommented middleware

--- index.js
+++ index.js
@@ -122,19 +122,19 @@ app.use('/verifyToken', JsonParser, (req,res)=> {
     }
 })
 
-//             MIDDLEWARE
-// app.use('/graphql', (req,res,next) =>{
+            MIDDLEWARE
+app.use('/graphql', (req,res,next) =>{
 
-//     const token = req.headers['authorization']
-//     try{
-//         req.user = verifyToken(token)
-//         next()
-//     } catch(er){
-//         res.status(401).json({
-//             message: er.message
-//         })
-//     }
-// })
+    const token = req.headers['authorization']
+    try{
+        req.user = verifyToken(token)
+        next()
+    } catch(er){
+        res.status(401).json({
+            message: er.message
+        })
+    }
+})
 
 app.use('/graphql', graphQLHTTP((req,res)=>({
     schema,
